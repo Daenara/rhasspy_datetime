44a39da4aa3519f1def5a5241ed36a4c40650e71
Daenara <nadine@daenara.net>
2021-05-08 01:26:54 +0200

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name='rhasspy_datetime',
     author='Daenara',
-    version='0.0.2',
+    version='0.0.3',
     url='https://github.com/Daenara/rhasspy_datetime',
     packages=setuptools.find_packages(),
     py_modules=["rhasspy_datetime", "data_types", "languages"],
